1adb75230c8ab0fc1547b32dd2f12963bb7583c1
Elias Gabriel <eliasgabrielcf@gmail.com>
2025-04-08 19:20:15 -0300

    Cobertura de testes com C8 - Parte 2

--- src/mathUtils.js
+++ src/mathUtils.js
@@ -4,3 +4,8 @@ export function sum(a, b) {
   }
   return a + b;
 }
+
+export function divide(a, b) {
+  if (b === 0) throw new Error('Division by zero');
+  return a / b;
+}
--- test/mathUtils.test.js
+++ test/mathUtils.test.js
@@ -1,5 +1,5 @@
 import { expect } from "chai"
-import { sum } from "../src/mathUtils.js"
+import { sum, divide } from "../src/mathUtils.js"
 
 describe('sum', () => {
   it('should add two numbers', () => {
@@ -8,5 +8,15 @@ describe('sum', () => {
 
   it('should throw error for non-number inputs', () => {
     expect(() => sum("2", 3)).to.throw('Invalid arguments');
+  });
+})
+
+describe('divide', () => {
+  it('should divide two numbers', () => {
+    expect(divide(6, 3)).to.equal(2);
+  });
+
+  it('should throw error for division by zero', () => {
+    expect(() => divide(6, 0)).to.throw('Division by zero');
   });
 })
